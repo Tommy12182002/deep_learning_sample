c0fb71c4ad9a8fa69fbcf346b5166e914d817bea
tommy12182002 <tommy12182002@gmail.com>
2017-12-09 19:13:25 +0900

    Add gradient two layer

--- common/layers.py
+++ common/layers.py
@@ -1,10 +1,14 @@
 # coding: utf-8
+import numpy as np
+import sys, os
+sys.path.append(os.pardir)
+from common.functions import *
 
 class Relu:
     def __init__(self):
         self.mask = None
 
-    def forword(self, x):
+    def forward(self, x):
         self.mask = (x <= 0)
         out       = x.copy()
         # trueになる要素にのみ0を代入する
@@ -55,4 +59,28 @@ class Affine:
         self.db = np.sum(dout, axis=0)
 
         dx = dx.reshape(*self.original_x_shape)  # 入力データの形状に戻す（テンソル対応）
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None # softmaxの出力
+        self.t = None # 教師データ
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        if self.t.size == self.y.size: # 教師データがone-hot-vectorの場合
+            dx = (self.y - self.t) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
+
         return dx
